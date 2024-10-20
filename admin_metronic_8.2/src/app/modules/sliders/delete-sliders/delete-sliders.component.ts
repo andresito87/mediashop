b150570dc1f10a9694a3d3835155e74cffed14de
Andres <andrespodadera87@gmail.com>
2024-10-20 21:51:51 +0200

    listing,editing and deleting sliders functionality finished

--- admin_metronic_8.2/src/app/modules/sliders/delete-sliders/delete-sliders.component.ts
+++ admin_metronic_8.2/src/app/modules/sliders/delete-sliders/delete-sliders.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { SlidersService } from '../service/sliders.service';
 
 @Component({
   selector: 'app-delete-sliders',
   templateUrl: './delete-sliders.component.html',
-  styleUrls: ['./delete-sliders.component.scss']
+  styleUrls: ['./delete-sliders.component.scss'],
 })
 export class DeleteSlidersComponent {
+  @Input() slider: any;
+  @Output() SliderDelete: EventEmitter<any> = new EventEmitter();
+  isLoading: any;
 
+  constructor(
+    public sliderService: SlidersService,
+    private toastr: ToastrService,
+    public modal: NgbActiveModal
+  ) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.isLoading = this.sliderService.isLoading$;
+  }
+
+  delete() {
+    this.sliderService.deleteSlider(this.slider.id).subscribe((res: any) => {
+      this.SliderDelete.emit({ message: 200 });
+      this.modal.close();
+    });
+  }
 }
--- admin_metronic_8.2/src/app/modules/sliders/edit-sliders/edit-sliders.component.ts
+++ admin_metronic_8.2/src/app/modules/sliders/edit-sliders/edit-sliders.component.ts
@@ -1,10 +1,95 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { SlidersService } from '../service/sliders.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-sliders',
   templateUrl: './edit-sliders.component.html',
-  styleUrls: ['./edit-sliders.component.scss']
+  styleUrls: ['./edit-sliders.component.scss'],
 })
 export class EditSlidersComponent {
+  title: string = '';
+  label: string = '';
+  subtitle: string = '';
+  link: string = '';
+  color: string = '';
+  state: number = 1;
+  image_preview: any =
+    'https://preview.keenthemes.com/metronic8/demo1/assets/media/svg/illustrations/easy/2.svg';
+  file_image: any = null;
+  isLoading$: any;
+  slider_id: string = '';
 
+  constructor(
+    public sliderService: SlidersService,
+    public toastr: ToastrService,
+    public activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.isLoading$ = this.sliderService.isLoading$;
+    this.activatedRoute.params.subscribe((res) => {
+      this.slider_id = res.id;
+    });
+    this.sliderService.showSlider(this.slider_id).subscribe((res: any) => {
+      this.title = res.slider.title;
+      this.label = res.slider.label;
+      this.subtitle = res.slider.subtitle;
+      this.link = res.slider.link;
+      this.color = res.slider.color;
+      this.state = res.slider.state;
+      this.image_preview = res.slider.image;
+    });
+  }
+
+  processFile($event: any) {
+    if ($event.target.files[0].type.indexOf('image') < 0) {
+      this.toastr.error('Validacion', 'El archivo no es una imagen');
+      return;
+    }
+
+    this.file_image = $event.target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(this.file_image);
+    reader.onloadend = () => (this.image_preview = reader.result);
+    this.isLoadingView();
+  }
+
+  isLoadingView() {
+    this.sliderService.isLoadingSubject.next(true);
+    setTimeout(() => {
+      this.sliderService.isLoadingSubject.next(false);
+    }, 50);
+  }
+
+  save() {
+    if (!this.title || !this.subtitle) {
+      this.toastr.error('Validacion', 'Los campos con el * son obligatorios');
+      return;
+    }
+    let formData = new FormData();
+    formData.append('title', this.title);
+    if (this.label) {
+      formData.append('label', this.label);
+    }
+    formData.append('subtitle', this.subtitle);
+    if (this.file_image) {
+      formData.append('imagen', this.file_image);
+    }
+    if (this.link) {
+      formData.append('link', this.link);
+    }
+    if (this.color) {
+      formData.append('color', this.color);
+    }
+
+    this.sliderService
+      .updateSliders(this.slider_id, formData)
+      .subscribe((res: any) => {
+        this.toastr.success('Exito', 'El carrusel se actualizó correctamente');
+      });
+  }
 }
