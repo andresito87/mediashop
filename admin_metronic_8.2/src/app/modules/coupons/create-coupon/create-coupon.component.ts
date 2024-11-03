f884fff7e3ef0fe235a7d43e7b807439b6ab7edf
Andres <andrespodadera87@gmail.com>
2024-11-03 15:24:39 +0100

    Frontend: create coupon component

--- admin_metronic_8.2/src/app/modules/coupons/create-coupon/create-coupon.component.ts
+++ admin_metronic_8.2/src/app/modules/coupons/create-coupon/create-coupon.component.ts
@@ -3,8 +3,52 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-coupon',
   templateUrl: './create-coupon.component.html',
-  styleUrls: ['./create-coupon.component.scss']
+  styleUrls: ['./create-coupon.component.scss'],
 })
 export class CreateCouponComponent {
+  code: any;
+  type_discount: number = 1; // 1 unlimited discounts, 2 limited discounts
+  discount: number = 0;
+  type_count: number = 1;
+  num_uses: number = 0;
+  type_coupon: number = 1;
+  product_id: any;
+  categorie_id: any;
+  brand_id: any;
 
+  isLoading$: any;
+
+  categories_first: any = [];
+  products: any = [];
+  brands: any = [];
+  products_add: any = [];
+  categories_add: any = [];
+  brands_add: any = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
+
+  changeTypeDiscount(value: number) {
+    this.type_discount = value;
+  }
+
+  changeTypeCount(value: number) {
+    this.type_count = value;
+  }
+
+  changeTypeCoupon(value: number) {
+    this.type_coupon = value;
+  }
+
+  save() {}
+
+  removeProduct(product: any) {}
+
+  removeCategorie(categorie: any) {}
+
+  removeBrand(brand: any) {}
 }
