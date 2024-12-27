472b9d7774862531137e4d26bee2c0122735730c
Andres <andrespodadera87@gmail.com>
2024-12-27 22:36:49 +0100

    Saving last changes for dockerice app

--- mediashop-frontend/src/app/app.config.server.ts
+++ mediashop-frontend/src/app/app.config.server.ts
@@ -1,7 +1,6 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { provideClientHydration } from '@angular/platform-browser';
 
 const serverConfig: ApplicationConfig = {
   providers: [provideServerRendering()],
--- mediashop-frontend/src/app/app.config.ts
+++ mediashop-frontend/src/app/app.config.ts
@@ -7,7 +7,10 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { provideToastr } from 'ngx-toastr';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { CookieService } from 'ngx-cookie-service';
 import { PermissionAuth } from './pages/auth/service/auth.guard';
 import { provideServerRendering } from '@angular/platform-server';
@@ -19,7 +22,9 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(), // required animations providers
     provideToastr(), // Toastr providers
     provideHttpClient(withFetch()),
-    provideClientHydration(), // HttpClient providers
+    provideClientHydration(
+      withEventReplay() // use hydration with v18 event replay
+    ),
     CookieService,
     PermissionAuth,
     provideServerRendering(),
