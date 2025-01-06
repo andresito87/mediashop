6d08fb43aed6f6258673164ad4d33555719f4a9a
Andres <andrespodadera87@gmail.com>
2025-01-06 11:10:22 +0100

    Documentation: adding documentation and more info to readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MediaShop: Proyecto de Tienda Virtual de tecnología
 
-Este proyecto es una tienda virtual de tecnología, en la cual se pueden comprar productos de tecnología, como ordenadores, móviles, tablets, etc.
+Este proyecto es una tienda virtual de tecnología, en la cual se pueden comprar productos como ordenadores, móviles, tablets, etc.
 
 ## Integrantes
 
@@ -11,14 +11,22 @@ Este proyecto es una tienda virtual de tecnología, en la cual se pueden comprar
 - HTML
 - CSS
 - JavaScript
+- TypeScript
 - PHP
 - MySQL
 
+## Herramientas
+
+- Visual Studio Code
+- Postman
+- GitHub
+- MySQL Workbench
+- Docker
+- Docker Compose
+
 ## Frameworks
 
-- Bootstrap
-- jQuery
-- Laravel
+- Laravel 11
 - Angular 18
 
 ## Funcionalidades
@@ -35,14 +43,42 @@ Este proyecto es una tienda virtual de tecnología, en la cual se pueden comprar
 
 ## Diagrama de clases
 
-## Diagrama de casos de uso
+[![Diagrama de clases](./BD/MediaShop%20ER.png)](./BD/MediaShop%20ER.png)
 
 ## Instalación
 
 1. Clonar el repositorio
+
+```bash
+git clone
+```
+
 2. Crear la base de datos
+
+```sql
+CREATE DATABASE mediashop;
+```
+
+```bash
+cd BD
+mysql -u root -p mediashop < mediashop.sql
+```
+
 3. Ejecutar Backend con Laravel (php artisan serve)
+
+```bash
+cd mediashop-backend
+composer install
+php artisan serve
+```
+
 4. Ejecutar Frontend con Angular (ng serve)
+
+```bash
+cd mediashop-frontend
+npm install
+ng serve
+```
 
 ## URLs de la aplicación
 
@@ -50,11 +86,13 @@ Este proyecto es una tienda virtual de tecnología, en la cual se pueden comprar
 - [Backend](http://localhost:8000/api)
 - [Admin](http://localhost:5000/auth/login)
 
-## Capturas de pantalla
-
 ## Finalidad
 
-Este proyecto es un proyecto de clase, por lo que no tiene fines comerciales. Se ha realizado con el fin de aprender a utilizar las tecnologías mencionadas anteriormente y ser presentado en clase como proyecto final de grado superior de Desarrollo de Aplicaciones Web.
+Este proyecto es un proyecto de clase, por lo que no tiene fines comerciales. Se ha realizado con el fin de aprender a utilizar las tecnologías y Frameworks mencionados anteriormente y ser presentado en clase como proyecto final de grado superior de Desarrollo de Aplicaciones Web.
+
+## API Postman Collection
+
+[<img src="https://run.pstmn.io/button.svg" alt="Run In Postman" style="width: 128px; height: 32px;">](https://god.gw.postman.com/run-collection/19407551-9c595361-10ab-4222-bba9-aa56cbf58ccc?action=collection%2Ffork&source=rip_markdown&collection-url=entityId%3D19407551-9c595361-10ab-4222-bba9-aa56cbf58ccc%26entityType%3Dcollection%26workspaceId%3De11487c7-189e-419d-8ceb-47ab3e2aea5f)
 
 ## Licencia
 
---
+++ mediashop-frontend/documentation/js/menu-wc.js
@@ -0,0 +1,192 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">mediashop-frontend documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
+                            </a>
+                        </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+                    </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/AddressProfileClientComponent.html" data-type="entity-link" >AddressProfileClientComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AppComponent.html" data-type="entity-link" >AppComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CartComponent.html" data-type="entity-link" >CartComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CheckoutComponent.html" data-type="entity-link" >CheckoutComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CodeForgotPasswordComponent.html" data-type="entity-link" >CodeForgotPasswordComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/EditProfileClientComponent.html" data-type="entity-link" >EditProfileClientComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FilterAdvanceProductComponent.html" data-type="entity-link" >FilterAdvanceProductComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FooterComponent.html" data-type="entity-link" >FooterComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ForgotPasswordComponent.html" data-type="entity-link" >ForgotPasswordComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HeaderComponent.html" data-type="entity-link" >HeaderComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HomeComponent.html" data-type="entity-link" >HomeComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LandingProductComponent.html" data-type="entity-link" >LandingProductComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LoginComponent.html" data-type="entity-link" >LoginComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LogoutComponent.html" data-type="entity-link" >LogoutComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ModalProductComponent.html" data-type="entity-link" >ModalProductComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/NewPasswordComponent.html" data-type="entity-link" >NewPasswordComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/OrdersProfileClientComponent.html" data-type="entity-link" >OrdersProfileClientComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/PasswordProfileClientComponent.html" data-type="entity-link" >PasswordProfileClientComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ProfileClientComponent.html" data-type="entity-link" >ProfileClientComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/RegisterComponent.html" data-type="entity-link" >RegisterComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ThankYouOrderComponent.html" data-type="entity-link" >ThankYouOrderComponent</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/AuthService.html" data-type="entity-link" >AuthService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/CartService.html" data-type="entity-link" >CartService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/HomeService.html" data-type="entity-link" >HomeService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/PermissionAuth.html" data-type="entity-link" >PermissionAuth</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/ProfileClientService.html" data-type="entity-link" >ProfileClientService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/UserAddressService.html" data-type="entity-link" >UserAddressService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/CartItem.html" data-type="entity-link" >CartItem</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
